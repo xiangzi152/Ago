3a300e243481da664983c47698b3fb54ae17896b
tianya <tianya152111@gmail.com>
2019-10-19 19:27:19 +0800

    add demo

---
+++ src/main/java/com/citi/demo/Demo.java
@@ -0,0 +1,11 @@
+package com.citi.demo;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("This is %d \r\n", i);
+            System.out.println("i = " + i);
+        }
+    }
+}
